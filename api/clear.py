526665d0aabac513d5d2c1c4e4f95e9445b1c972
D111sP <115263492+D111sP@users.noreply.github.com>
2023-10-14 05:38:26 +0500

    добавил офиссы и загружиность

--- api/clear.py
+++ api/clear.py
@@ -1,19 +1,51 @@
-k = 15
-o = 22
+# k = 15
+# o = 22
+#
+# cel = (o // k)
+# ost = 0 if o%k == 0 else 1
+#
+# if k * 1.5 >= o:
+#     st = "green"
+# elif k * 2.5 >= o:
+#     st = "yellow"
+# elif k * 3.5 >= o:
+#     st = "orange"
+# else:
+#     st = "red"
+#
+# print(st)
+#
+#
+# print((cel+ost)*10)
+import math
 
-cel = (o // k)
-ost = 0 if o%k == 0 else 1
 
-if k * 1.5 >= o:
-    st = "green"
-elif k * 2.5 >= o:
-    st = "yellow"
-elif k * 3.5 >= o:
-    st = "orange"
-else:
-    st = "red"
+def haversine(lat1, lon1, lat2, lon2):
+    # Радиус Земли в километрах
+    R = 6371.0
 
-print(st)
+    # Переводим градусы в радианы
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
 
+    # Разница в широте и долготе
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
 
-print((cel+ost)*10)
+    # Вычисляем расстояние с использованием формулы Гаверсинуса
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+
+    return distance
+
+# Пример использования 'latitude': 55.880677, 'longitude': 37.558643, 'latitude': 55.745726, 'longitude': 37.625702,
+lat1 = 55.880677
+lon1 = 37.558643
+lat2 = 55.745726
+lon2 = 37.625702
+
+distance = haversine(lat1, lon1, lat2, lon2)
+print(f"Расстояние между точками: {distance} км")
--- api/server_api_vtb.py
+++ api/server_api_vtb.py
@@ -1,14 +1,106 @@
+import random
+
 from fastapi import FastAPI
 from pydantic import BaseModel
-from config import bankomat, offcie
+from config import bankomats, offices
+import math
 
 app = FastAPI()
 
 
 @app.get("/get_atm")
 async def process_data():
-    return bankomat
+    return bankomats
 
+class RequestData(BaseModel):
+    latitude: float
+    longitude: float
+@app.post("/get_atm_location")
+async def process_data(data: RequestData):
+    latitude_people = data.latitude
+    longitude_people = data.longitude
+
+    points_nearby = []
+    len_points_nearby = []
+    for bankomat in bankomats:
+        ans = haversine(latitude_people, longitude_people, bankomat['latitude'], bankomat['longitude'])
+        if ans < 15.0:
+            bankomat['lenth'] = ans
+            if len(points_nearby) == 5:
+                for i in range(len(len_points_nearby)):
+                    if len_points_nearby[i] > ans:
+                        len_points_nearby[i] = ans
+                        points_nearby[i] = bankomat
+                        break
+            else:
+                points_nearby.append(bankomat)
+                len_points_nearby.append(ans)
+
+    return points_nearby
+
+@app.post("/get_office_location")
+async def process_data(data: RequestData):
+    latitude_people = data.latitude
+    longitude_people = data.longitude
+
+    points_nearby = []
+    len_points_nearby = []
+
+    for bankomat in bankomats:
+        ans = haversine(latitude_people, longitude_people, bankomat['latitude'], bankomat['longitude'])
+        if ans < 15.0:
+            bankomat['lenth'] = ans
+            if len(points_nearby) == 5:
+                for i in range(len(len_points_nearby)):
+                    if len_points_nearby[i] > ans:
+                        len_points_nearby[i] = ans
+                        points_nearby[i] = bankomat
+                        break
+            else:
+                points_nearby.append(bankomat)
+                len_points_nearby.append(ans)
+
+    for point in points_nearby:
+        if 'loadingQueue' not in point:
+            point['loadingQueue'] = {}
+
+        for el in ['credit', 'card', 'mortgage', 'payments']:
+            k, o = random.randint(5, 15), random.randint(0, 50)
+            cel = (o // k)
+            ost = 0 if o%k == 0 else 1
+
+            if k * 1.5 >= o:
+                st = "green"
+            elif k * 2.5 >= o:
+                st = "yellow"
+            elif k * 3.5 >= o:
+                st = "orange"
+            else:
+                st = "red"
+            point['loadingQueue'][el] = [(cel+ost)*10, st]
+
+    return points_nearby
+    #loadingQueue credit card mortgage payments
+def haversine(lat_p, lon_p, lat_vtb, lon_vtb):
+    # Радиус Земли в километрах
+    R = 6371.0
+
+    # Переводим градусы в радианы
+    lat1 = math.radians(lat_p)
+    lon1 = math.radians(lon_p)
+    lat2 = math.radians(lat_vtb)
+    lon2 = math.radians(lon_vtb)
+
+    # Разница в широте и долготе
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    # Вычисляем расстояние с использованием формулы Гаверсинуса
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+
+    return distance
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
