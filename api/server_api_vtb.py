e21c93c85fb63be636f2ecc8103964aae9c27662
kek4ok <39804639+kek4ok@users.noreply.github.com>
2023-10-14 03:18:18 +0300

    ))))))))

--- api/server_api_vtb.py
+++ api/server_api_vtb.py
@@ -1,14 +1,31 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from config import bankomat, offcie
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/get_atm")
 async def process_data():
-    return bankomat
+    return bankomat[:10]
+
 
 if __name__ == '__main__':
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
