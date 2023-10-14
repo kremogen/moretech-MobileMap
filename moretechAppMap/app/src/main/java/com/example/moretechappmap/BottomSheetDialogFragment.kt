a9dd4508903ed6d6d785a5c4867cf190b9e49f3a
Kremogen <nik.kosachev@mail.ru>
2023-10-14 21:21:20 +0300

    main project

---
+++ moretechAppMap/app/src/main/java/com/example/moretechappmap/BottomSheetDialogFragment.kt
@@ -0,0 +1,16 @@
+package com.example.moretechappmap
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class MyBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottom_sheet_layout, container, false)
+    }
+}
