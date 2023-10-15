573204750720ac7135e8b0da54dab3704549aeae
kek4ok <39804639+kek4ok@users.noreply.github.com>
2023-10-15 06:18:45 +0300

    HyPe

---
+++ kotlin_app/map-with-placemark/src/main/kotlin/com/yandex/mapkitdemo/placemark/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.yandex.mapkitdemo.placemark
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    private val splashDelay = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, Activity::class.java)
+            startActivity(intent)
+        }, splashDelay.toLong())
+    }
+}
