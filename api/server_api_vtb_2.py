be1b9d8ac3c2215b90ba687005b7b9ad3c295329
D111sP <115263492+D111sP@users.noreply.github.com>
2023-10-15 07:33:47 +0500

    1 км диаметр

--- api/server_api_vtb_2.py
+++ api/server_api_vtb_2.py
@@ -10,8 +10,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
-lat_r = 0.01585
-lon_r = 0.01125
+lat_r = 0.00785
+lon_r = 0.00525
 
 app = FastAPI()
 origins = ['*']
