6e73a50789a1c28695bf9475defaabe26641406c
hteppl <hteppl.dev@gmail.com>
2023-10-15 05:34:23 +0300

    Divide deltas by 2

--- api/server_api_vtb_2.py
+++ api/server_api_vtb_2.py
@@ -10,8 +10,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
-lat_r = 0.01585
-lon_r = 0.01125
+lat_r = 0.01585 * 0.5
+lon_r = 0.01125 * 0.5
 
 app = FastAPI()
 origins = [
