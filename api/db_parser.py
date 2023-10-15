d7339296f23dc9c3a7e21b1ef10f3ddf9c27af71
hteppl <hteppl.dev@gmail.com>
2023-10-15 03:12:26 +0300

    Add x and y in database

--- api/db_parser.py
+++ api/db_parser.py
@@ -1,3 +1,4 @@
+import math
 import sqlite3
 
 import config
@@ -6,8 +7,9 @@ sqlite_create = '''CREATE TABLE IF NOT EXISTS atm_data (id INTEGER PRIMARY KEY, 
     latitude FLOAT, longitude FLOAT, all_day BIT);'''
 
 sqlite_create0 = '''CREATE TABLE IF NOT EXISTS office_data (id INTEGER PRIMARY KEY, salePointName VARCHAR,
-    address VARCHAR, latitude FLOAT, longitude FLOAT, distance INTEGER, rko VARCHAR, officeType VARCHAR,
-    salePointFormat VARCHAR, suoAvailability BIT, hasRamp BIT, metroStation VARCHAR, kep VARCHAR);'''
+    address VARCHAR, latitude FLOAT, longitude FLOAT, distance INTEGER, x FLOAT, y FLOAT,
+    rko VARCHAR, officeType VARCHAR, salePointFormat VARCHAR, suoAvailability BIT, hasRamp BIT,
+    metroStation VARCHAR, kep VARCHAR);'''
 
 
 def get_connection():
@@ -39,13 +41,18 @@ def parse0():
         cursor = connection.cursor()
         for data in config.offices:
             query = """INSERT INTO `office_data` (`salePointName`, `address`, `latitude`,
-            `longitude`, `distance`, `rko`, `officeType`, `salePointFormat`, `suoAvailability`,
-            `hasRamp`, `metroStation`, `kep`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?);"""
+            `longitude`, `distance`, `x`, `y`, `rko`, `officeType`, `salePointFormat`, `suoAvailability`,
+            `hasRamp`, `metroStation`, `kep`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?);"""
+            
+            x, y = lon_lat_to_mercator(data['longitude'], data['latitude'])
+            
             cursor.execute(query, (
                 data['salePointName'], data['address'], data['latitude'], data['longitude'], data['distance'],
+                x, y,
                 'Неизвестно' if data['rko'] is None else data['rko'].capitalize(),
                 data['officeType'].capitalize(), data['salePointFormat'],
-                'Есть' if data['suoAvailability'] == 'Y' else ('Нет' if data['suoAvailability'] == 'N' else 'Неизвестно'),
+                'Есть' if data['suoAvailability'] == 'Y' else (
+                    'Нет' if data['suoAvailability'] == 'N' else 'Неизвестно'),
                 'Есть' if data['hasRamp'] == 'Y' else ('Нет' if data['hasRamp'] == 'N' else 'Неизвестно'),
                 'Нет' if data['metroStation'] is None else data['metroStation'],
                 'Есть' if data['kep'] is True else ('Нет' if data['kep'] is False else 'Неизвестно')
@@ -55,6 +62,13 @@ def parse0():
         connection.close()
     except (Exception, sqlite3.Error) as error:
         print("Error while getting data", error)
+
+
+def lon_lat_to_mercator(lon, lat):
+    R = 6371000
+    x = R * math.radians(lon)
+    y = R * math.log(math.tan(math.pi / 4 + math.radians(lat) / 2))
+    return x, y
 
 
 con = get_connection()
